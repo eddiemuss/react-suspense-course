50cc6fccf0235fa71f0639faf6b7a5ad2b839825
eddie <eddimuss@web.de>
2020-02-06 02:27:15 +0100

    Add lazy loading as dynamic file import and supsense as promise handler

--- src/lessons/101/app.js
+++ src/lessons/101/app.js
@@ -1,12 +1,12 @@
 import React from "react";
-// 1. Change this static import to a dynamic import, wrapped in React.lazy
-import PokemonDetail from "./pokemon-detail";
+const PokemonDetail = React.lazy(() => import('./pokemon-detail'));
 
 export default function App() {
   return (
     <div>
-      {/* 2. Wrap this component in a React.Suspense component with fallback */}
-      <PokemonDetail />
+      <React.Suspense fallback={<p>Loading ...</p>}>
+        <PokemonDetail />
+      </React.Suspense>
     </div>
   );
 }
