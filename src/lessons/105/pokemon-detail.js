40f8b14daa7b611abd628d65dbe65a7797bdbd28
eddie <eddimuss@web.de>
2020-02-08 00:28:32 +0100

    create a supens wrapper to handle all three states of a promise

--- src/lessons/105/pokemon-detail.js
+++ src/lessons/105/pokemon-detail.js
@@ -1,46 +1,29 @@
 import React from "react";
+import { fetchPokemon, suspensify } from './api';
 
-function suspensify(promise) {
-  let status = "pending";
-  let result;
-  let suspender = promise.then(
-    response => {
-      status = "success";
-      result = response;
-    },
-    error => {
-      status = "error";
-      result = error;
-    }
-  );
-
-  return {
-    read() {
-      if (status === "pending") {
-        throw suspender;
-      }
-      if (status === "error") {
-        throw result;
-      }
-      if (status === "success") {
-        return result;
-      }
-    }
-  };
-}
-
+const initialPokemon = suspensify(fetchPokemon(1));
 // 2. Rename the `pokemon` variable to `initialPokemon` to indicate that it is only the first
-let pokemon = suspensify(
-  fetch(`https://pokeapi.co/api/v2/pokemon/1`).then(res => res.json())
-);
 
 export default function PokemonDetail() {
   // 1. Use React.useState to track the current PokemonResource and setPokemonResource
+  const [pokemonResource, setPokemonResource] = React.useState(initialPokemon);
   // 2. (see above)
   // 3. Provide `initialPokemon` to `React.useState` as default
   // 4. Create an intermediate `pokemon` variable that `read()`s the `pokemonResource`
+  const pokemon = pokemonResource.read();
   // 5. Create "Next" button
   //  * When clicked, call `setPokemonResource`
   //  * Use suspensify(fetchPokemon(...)) to fetch the pokemon with the next id
-  return <div>{pokemon.read().name}</div>;
+  return (
+    <>
+      <div>{pokemon.name}</div>
+      <button type="button" onClick={() => {
+        setPokemonResource(
+          suspensify(
+            fetchPokemon(pokemon.id + 1)
+          )
+        )
+      }}>Next</button>
+    </>
+  );
 }
