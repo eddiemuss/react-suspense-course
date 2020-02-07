4239fb818c053d9fa6726f0fb06ee8bdd149de22
eddie <eddimuss@web.de>
2020-02-07 19:36:10 +0100

    Add error boundary

--- src/lessons/102/app.js
+++ src/lessons/102/app.js
@@ -1,19 +1,17 @@
 import React from "react";
-// 1. Create an import error by giving React.lazy a Promise.reject()
-const PokemonDetail = React.lazy(() => import("./pokemon-detail"));
-
-// 2. Check the console for errors about error boundaries
-
-// 3-5. Copy/Paste an ErrorBoundary declaration here and configuer with a fallback= prop
-//    https://reactjs.org/docs/error-boundaries.html
+import ErrorBoundary from './ErrorBoundary';
+const PokemonDetail = React.lazy(() => Promise.reject());
+// const PokemonDetail = React.lazy(() => import("./pokemon-detail"));
 
 export default function App() {
   return (
     <div>
-      {/* 6. Wrap your Suspense Component in the freshly minted ErrorBoundary component */}
-      <React.Suspense fallback={"Loading your Pokemon..."}>
-        <PokemonDetail />
-      </React.Suspense>
+      <h1>Pokedex</h1>
+      <ErrorBoundary>
+        <React.Suspense fallback={"Loading your Pokemon..."}>
+          <PokemonDetail />
+        </React.Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
