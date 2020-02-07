8512ae7486d562f55478119e75a612bba2eb5a26
eddie <eddimuss@web.de>
2020-02-07 19:43:41 +0100

    tested promise statuses

--- src/lessons/103/app.js
+++ src/lessons/103/app.js
@@ -3,10 +3,18 @@ import ErrorBoundary from "./error-boundary";
 
 // Test the three states we're setup for
 // 1. Error: return a rejected promise to React.lazy to enact the ErrorBoundary fallback
+// const PokemonDetail = React.lazy(() => Promise.rejected());
+
 // 2. Pending: return a new pending promise to React.lazy to enact the Suspense fallback
+// const PokemonDetail = React.lazy(() => new Promise(
+//   resolve => setTimeout(() => {
+//     resolve()
+//   }, 1000)
+// ));
+
 // 3. Success: return a new pending promise that resolves a module after a timeout to React.lazy to enact the Suspense fallback
 //    Or just fix it to import properly `pokemon-default` module.
-const PokemonDetail = React.lazy(() => Promise.reject());
+const PokemonDetail = React.lazy(() => import('./pokemon-detail'));
 
 export default function App() {
   return (
